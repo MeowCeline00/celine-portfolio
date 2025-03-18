654e96088841fdbc4fce1e3ba3a962292d97b7b0
Celine_00 <132836456+MeowCeline00@users.noreply.github.com>
2025-03-18 10:05:00 -0700

    fix some styling on the about me page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Preloader from "./components/preloader/Preloader";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact"
 import Footer from "./components/footer/Footer";
 import { ThemeContext } from "./Context/theme";
 import {
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -8,32 +8,35 @@ import "../About/about.css";
 import { AiFillGithub, AiFillInstagram, AiOutlineMail } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
-
 function Home2() {
   return (
     <Container fluid className="home-about-section" id="about">
-      <Container>
-        <Row>
-          <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }} data-aos="fade-right">
-              <span className="primary-header"> ABOUT </span> ME
+      <Container className="content-container">
+        <Row className="section-header">
+          <Col className="text-center">
+            <h1 data-aos="fade-right">
+              <span className="primary-header">ABOUT</span> ME
             </h1>
+          </Col>
+        </Row>
+        
+        <Row className="justify-content-center">
+          <Col lg={6} md={9} sm={12} className="home-about-description">
             <p className="home-about-body" data-aos="fade-up">
-              <br />
-              Hi, I’m Celine—a digital designer and front-end developer based in
-              Vancouver, BC, Canada. I’m currently in my 4th term of the Digital
+              Hi, I'm Celine—a digital designer and front-end developer based in
+              Vancouver, BC, Canada. I'm currently in my 4th term of the Digital
               Design and Development (D3) Program at BCIT. Before diving into
-              the world of design and development, I earned a Bachelor’s degree
+              the world of design and development, I earned a Bachelor's degree
               in Biology from the University of British Columbia. My scientific
               background honed my research and analytical skills, giving me a
               unique perspective when approaching challenges and working with
-              data. I’m passionate about web development, graphic design, and
-              photography, and I’m always eager to learn new skills and explore
-              fresh perspectives. If you’d like to connect, feel free to reach
-              out—I’m always happy to make new friends and collaborate!
+              data. I'm passionate about web development, graphic design, and
+              photography, and I'm always eager to learn new skills and explore
+              fresh perspectives. If you'd like to connect, feel free to reach
+              out—I'm always happy to make new friends and collaborate!
             </p>
           </Col>
-          <Col md={4} className="myAvtar">
+          <Col lg={4} className="d-none d-lg-block myAvtar">
             <div className="avatar-container">
               <img
                 data-aos="fade-left"
@@ -45,39 +48,47 @@ function Home2() {
           </Col>
         </Row>
 
-        <Row>
-          <Col md={11} className="home-about-description-part">
-            <h1 style={{ fontSize: "2.6em" }} data-aos="fade-right">
-              <span className="primary-header"> Skillset </span> I Work With
+        <Row className="section-header">
+          <Col className="text-center">
+            <h1 data-aos="fade-right">
+              <span className="primary-header">Skillset</span> I Work With
             </h1>
-            <br />
+          </Col>
+        </Row>
+        
+        <Row className="justify-content-center">
+          <Col lg={10} md={11} className="tech-icons-container">
             <Techstack />
           </Col>
         </Row>
 
-        <Row>
-          <Col md={11} className="home-about-description-part">
-            <h1 style={{ fontSize: "2.6em" }} data-aos="fade-right">
-              <span className="primary-header"> Tools </span> I Use
+        <Row className="section-header">
+          <Col className="text-center">
+            <h1 data-aos="fade-right">
+              <span className="primary-header">Tools</span> I Use
             </h1>
-            <br />
+          </Col>
+        </Row>
+        
+        <Row className="justify-content-center">
+          <Col lg={10} md={11} className="tools-icons-container">
             <Toolstack />
           </Col>
         </Row>
 
-        <Row>
+        <Row className="section-header">
           <Col md={12} className="home-about-social">
-            <h1 data-aos="fade-right">
-              <span className="primary-header">CONNECT </span> WITH ME
+            <h1 data-aos="fade-right" className="text-center">
+              <span className="primary-header">CONNECT</span> WITH ME
             </h1>
-            <p data-aos="fade-left">Feel free to connect with me</p>
+            <p data-aos="fade-left" className="text-center">Feel free to connect with me</p>
             <ul className="home-about-social-links" data-aos="fade-up">
               <li className="social-icons">
                 <a
                   href="https://github.com/MeowCeline00"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                   aria-label="github"
                 >
                   <AiFillGithub />
@@ -89,7 +100,7 @@ function Home2() {
                   href="https://www.linkedin.com/in/congling-wang-22a475172/"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                   aria-label="linkedin"
                 >
                   <FaLinkedinIn />
@@ -117,7 +128,6 @@ function Home2() {
                   <AiOutlineMail />
                 </a>
              </li>
-             
             </ul>
           </Col>
         </Row>
@@ -125,4 +135,5 @@ function Home2() {
     </Container>
   );
 }
+
 export default Home2;
